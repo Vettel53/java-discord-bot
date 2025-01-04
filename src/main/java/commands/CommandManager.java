6dbf143216aa96682c85bf8ed1075fd243c77b55
Javier <62152194+Vettel53@users.noreply.github.com>
2025-01-03 22:36:51 -0600

    Information Command
    - Started implementation on Information Command.
    
    - Information command will have miscellaneous data about computer bot is running on etc.

--- src/main/java/commands/CommandManager.java
+++ src/main/java/commands/CommandManager.java
@@ -65,6 +65,11 @@ public class CommandManager extends ListenerAdapter {
             case "cat":
                 CatCommand.handleCatCommand(event, embed);
                 break;
+            case "statistics":
+                InformationCommand infoCommand = new InformationCommand();
+                infoCommand.handleInformationCommand(event, embed);
+                break;
+            // Add more commands here...
             default:
                 event.reply("Unknown command...").queue();
                 break;
@@ -89,6 +94,8 @@ public class CommandManager extends ListenerAdapter {
                             .queue();
             testingGuild.upsertCommand("cat", "Random cat photo!")
                             .queue();
+            testingGuild.upsertCommand("statistics", "Statistics about Brownseal!")
+                    .queue();
             testingGuild.upsertCommand("fortnite", "Fortnite Commands!")
                     .addSubcommands(new SubcommandData ("stats", "Search a player's stats!")
                             // addOption names must be lowercase and one word
--- src/main/java/commands/InformationCommand.java
+++ src/main/java/commands/InformationCommand.java
@@ -1,7 +1,63 @@
 package commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+
+import java.awt.*;
+import java.time.Instant;
+
+import static config.BotConstants.BOT_IMAGE_URL;
+import static config.BotConstants.BOT_URL;
+
 public class InformationCommand {
+    private static final long BYTES_TO_MB = 1024 * 1024; // Used to calculate the bytes to megabytes
+    private final long maxMemory;
+    private final long allocatedMemory;
+    private final long freeMemory;
+    private final int activeThreads;
+    private final int availableProcessors;
 
+    // Using instance variables to avoid "race conditions" and shared variables
+    public InformationCommand() {
+        this.maxMemory = Runtime.getRuntime().maxMemory();
+        this.allocatedMemory = Runtime.getRuntime().totalMemory();
+        this.freeMemory = Runtime.getRuntime().freeMemory();
+        this.activeThreads = Thread.activeCount();
+        this.availableProcessors = Runtime.getRuntime().availableProcessors();
+    }
 
-    // TODO: possible /information command to see information of bot
+    public void handleInformationCommand(SlashCommandInteraction event, EmbedBuilder embed) {
+
+        try {
+            String response = formatInformationResponse();
+
+            embed.setAuthor("Brownseal Statistics", BOT_URL, BOT_IMAGE_URL );
+            embed.setTitle("Interesting Information!");
+            embed.setDescription(response);
+            embed.setColor(new Color(56, 52, 250));
+            embed.setFooter("Vettel53");
+            embed.setTimestamp(Instant.now());
+            event.replyEmbeds(embed.build()).queue();
+            embed.clear();
+        } catch (Exception e) {
+            event.reply("Error fetching information " + e.getMessage())
+                    .queue();
+        }
+
+    }
+
+    public String formatInformationResponse() {
+
+        String formattedResponse = "**OS Name: **" + System.getProperty("os.name") + "\n\n"
+                + "**OS Version: **" + System.getProperty("os.version") + "\n"
+                + "**Java Version: **" + System.getProperty("java.version")  + "\n"
+                + "**Max Memory: **" + maxMemory / BYTES_TO_MB + " MB" + "\n"
+                + "**Allocated Memory: **" + allocatedMemory / BYTES_TO_MB + " MB" + "\n"
+                + "**Free Memory: **" + freeMemory / BYTES_TO_MB + " MB" + "\n"
+                + "**Thread Count: **" + activeThreads + "\n"
+                + "**Processor Count: **" + availableProcessors + "\n";
+
+        return formattedResponse;
+    }
+
 }
